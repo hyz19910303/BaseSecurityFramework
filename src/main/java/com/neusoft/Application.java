5b28ffb35fc61e9676d36ce669ce352d15e892b1
hyz19910303 <565796351@qq.com>
2018-07-31 21:10:48 +0800

    init

---
+++ src/main/java/com/neusoft/Application.java
@@ -0,0 +1,14 @@
+package com.neusoft;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.neusoft.dao")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
