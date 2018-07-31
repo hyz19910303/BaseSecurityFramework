bf299963ea49c486ac9c54bf7f06765cdb0c6ae2
hyz19910303 <565796351@qq.com>
2018-07-31 21:10:17 +0800

    init

---
+++ src/main/java/com/neusoft/utils/validator/group/UpdateGroup.java
@@ -0,0 +1,12 @@
+package com.neusoft.utils.validator.group;
+
+/**
+ * 更新数据 Group
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-15 21:21
+ */
+
+public interface UpdateGroup {
+
+}
