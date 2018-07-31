8511ef6fa8e72d2788f3f3c85da55c03d069fa67
hyz19910303 <565796351@qq.com>
2018-07-31 21:07:17 +0800

    init

---
+++ src/main/java/com/neusoft/dao/ScheduleJobDao.java
@@ -0,0 +1,20 @@
+package com.neusoft.dao;
+
+import java.util.Map;
+
+import com.neusoft.entity.ScheduleJobEntity;
+
+/**
+ * 定时任务
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2016年12月1日 下午10:29:57
+ */
+public interface ScheduleJobDao extends BaseDao<ScheduleJobEntity> {
+	
+	/**
+	 * 批量更新状态
+	 */
+	int updateBatch(Map<String, Object> map);
+}
